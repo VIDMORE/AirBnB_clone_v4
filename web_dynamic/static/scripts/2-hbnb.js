53cc7acf211cd30c8af8f73e1d1c016754d0366b
David Corredor <vidmore8@gmail.com>
2020-09-25 14:49:20 -0500

    [UPLOAD] - JS get query

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+$(function () {
+  const amenities = {};
+  $('.inp_amenity').change(function(e) {
+    if (this.checked) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      console.log(amenities)
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities h4').text(Object.values(amenities).join(', '));
+  });
+
+  $.get('http://127.0.0.1:5001/api/v1/status/', (data, status) => {
+    if (data.status === "OK") {
+      $('#api_status').addClass('available')
+    }
+    else {
+      $('#api_status').removeClass('available')
+    }
+  });
+
+});
